edf5f9857c453ced5dafb102ca9aaa2a4640b2c2
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2011-11-16 12:32:47 +0200

    Fixed missing flowspec classes from __init__.py. Added classes to import statement

--- nxpy/__init__.py
+++ nxpy/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from device_proxy import Device, DeviceDiff, Interface, Vlan, Unit, Parser
+from device_proxy import Device, DeviceDiff, Interface, Vlan, Unit, Flow, Route, Parser
 
 __all__ = [
 "Device",
